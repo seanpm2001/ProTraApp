1fd5408d016935146ed65196dee2ac578ea695a5
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-07 18:49:47 -0800

    Main_V1.php
    An archive of the first version of the main PHP script for this directory.

---
+++ LIBrary/WebBrowsers/HTML5/WHATWG/PHP/OldVersions/Main_V1.php
@@ -0,0 +1,16 @@
+<?php
+# Start of script
+// I am not very good at PHP, this is just a sample script at the moment
+$options = function(return platform);
+$redirectTo = function(return options);
+$YES = function(redirectTo:"/installPath/");;
+$YES.INSTALL = function(redirectTo:"/appstore/redirectTunnel/");
+$NO = function(exit);
+$NO.INSTALL = function(exit);
+/* File info
+File type: PHP script file (*.php)
+File version: 1 (Sunday, March 7th 2021 at 6:49 pm)
+Line count (including blank line and compiler line): 17
+*/
+# End of script
+?>
