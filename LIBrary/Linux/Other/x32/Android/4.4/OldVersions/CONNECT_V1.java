77e8e12ed08fcf70318f0f5d9701892de1d1f7f3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-07 19:42:20 -0800

    CONNECT_V1.java
    An archive of the first version of the connection script for Android KitKat.

---
+++ LIBrary/Linux/Other/x32/Android/4.4/OldVersions/CONNECT_V1.java
@@ -0,0 +1,30 @@
+// Start of script
+// Connecting 2 Windows 10 apps together (2004)
+// Definitions
+// Different steps are required for Android. This script may not work correctly.
+import java.util.scanner; // Imports the input methods for Java
+class android32Version(4.4)) // Trying to form a class here, so it can detect from different Android 4.4 versions.
+appName1 = str("Enter app name here (the current one."); // Modify this line for your program
+appName2 = str("Enter second app name here (the one you are linking to."); // Modify this line as well for your program
+int ynInput; // The input method
+app2URL = str("/storage/emulated/0/") // Incomplete. This method is for linking the 2 apps together, and jumping to the other program. Will prompt an install notice if it fails to reach. If the program is installed but reaching it results in an error, that means that ProTraApp was configured incorrectly
+// Main
+public class Program
+{
+    public static void Main(string[] args) {
+      System.out.println("Switch programs/apps?\nThis application wants to jump from " + appName1() + " to " + appName2() + " Do you wish to proceed? (Y/N)");
+    // ynInput = getChar( );
+    scanf("%s %d", str, &ynInput);
+    if (ynInput = "y" or "Y"); {
+      return app2URL();
+}}}
+    else: {
+      quit(); // Exits the prompt
+}} // End of main method
+/* File info
+* File version: `1 (Sunday, March 7th 2021 at 7:40 pm)
+* File type: Java Source file (*.java)
+* Line count (including blank lines and compiler line): 31
+*/
+// This script is incomplete, and needs lots of work
+// End of script
