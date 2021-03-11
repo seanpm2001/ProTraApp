cf4f5bc1054b9ac46c5bdaca5cb3f6a2585e2c5b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-10 20:12:16 -0800

    CONNECT.cpp
    The connection script for Fedora 21.

---
+++ LIBrary/Linux/x64/RPM/Fedora/21/C++/CONNECT.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+// Start of script
+// Connecting 2 Fedora 21 apps together (64 bit)
+// Definitions
+// Notice: I am very inexperienced in C++ so this script likely doesn't work. It is just a demo script at the moment.
+class fedora(int(21.86.64)); { // Trying to form a class here, so it can detect from different Fedora versions.
+appName1 = char("Enter app name here (the current one."); // Modify this line for your program
+appName2 = char("Enter second app name here (the one you are linking to."); // Modify this line as well for your program
+int ynInput; // The input method
+app2URL = char("/home/<"); // Incomplete. This method is for linking the 2 apps together, and jumping to the other program. Will prompt an install notice if it fails to reach. If the program is installed but reaching it results in an error, that means that ProTraApp was configured incorrectly
+exit = endProgram // I don't know how to end the program in C
+// Main
+int main()
+    std::cout << "Switch programs/apps?\nThis application wants to jump from " + appName1() + " to " + appName2() + " Do you wish to proceed? (Y/N)";
+    ynInput = getChar( );
+    scanf("%s %d", str, &ynInput);
+    if (ynInput = "y" or "Y"); {
+      return app2URL
+    }
+    else: {
+      return exit // Exits the prompt
+}} // End of main method
+/* File info
+* File version: `1 (Wednesday, March 10th 2021 at 8:12 pm)
+* File type: C Source file (*.c)
+* Line count (including blank lines and compiler line): 30
+*/
+// This script is incomplete, and needs lots of work
+// End of script
