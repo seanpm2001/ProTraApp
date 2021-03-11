93ab2e81794a45978976aceba6973935e39bc284
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-10 19:18:17 -0800

    CONNECT.c
    The connection script for Fedora 32.

---
+++ LIBrary/Linux/x64/RPM/Fedora/32/C/CONNECT.c
@@ -0,0 +1,29 @@
+  #include <stdio.h>
+// Start of script
+// Connecting 2 Fedora 32 apps together (64 bit)
+// Definitions
+union fedora(int(32.86.64)) // Trying to form a class here, so it can detect from different Fedora versions.
+appName1 = char("Enter app name here (the current one."); // Modify this line for your program
+appName2 = char("Enter second app name here (the one you are linking to."); // Modify this line as well for your program
+int ynInput; // The input method
+app2URL = char("/home/<") // Incomplete. This method is for linking the 2 apps together, and jumping to the other program. Will prompt an install notice if it fails to reach. If the program is installed but reaching it results in an error, that means that ProTraApp was configured incorrectly
+exit = endProgram // I don't know how to end the program in C
+// Main
+int main(void) {
+    printf("Switch programs/apps?\nThis application wants to jump from " + appName1() + " to " + appName2() + " Do you wish to proceed? (Y/N)");
+    // ynInput = getChar( );
+    scanf("%s %d", str, &ynInput);
+    if (ynInput = "y" or "Y"); {
+      return app2URL
+    }
+    else: {
+      return exit // Exits the prompt
+}} // End of main method
+
+/* File info
+* File version: `1 (Wednesday, March 10th 2021 at 7:18 pm)
+* File type: C Source file (*.c)
+* Line count (including blank lines and compiler line): 30
+*/
+// This script is incomplete, and needs lots of work
+// End of script
