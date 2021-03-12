f891e4f750e369c33a8112c276811081fcf22ef7
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-11 20:27:19 -0800

    CONNECT.m
    The connection script for iOS 4.

---
+++ LIBrary/Darwin/i/OS/4/Objective-C/CONNECT.m
@@ -0,0 +1,36 @@
+@interface classname : superclasname {
+  // Start of script
+  // instance variables
+  + classMethod1;
+  + (return_type)classMethod2;
+  + (return_type)classMethod3:(param1_type)param1_varName;
+  - (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+  - (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName;
+param2_callName:(param2_type)param2_varName;
+  @MiOS4_ObjectiveC classname : superclassname {
+    // Start of script
+    // Warning: MacOS and iOS programming differs. This script has a higher chance of not working due to non-existant mobile libraries
+    // Connecting iOS 4 applications together. Note that I have very little experience with Objective-C, so this script likely doesn't work correctly, and is currently just a demo script
+    //Definitions
+    appName1 = string("Enter app name here (the current one."); // Modify this line for your program
+    appName2 = string("Enter second app name here (the one you are linking to."); // Modify this line as well for your program
+    integer ynInput; // The input method
+    app2URL = string("/I don't know the iPhoneOS File path/"); // Incomplete. This method is for linking the 2 apps together, and jumping to the other program. Will prompt an install notice if it fails to reach. If the program is installed but reaching it results in an error, that means that ProTraApp was configured incorrectly
+    // Main
+    @switchPrograms
+      printf ("Switch programs/apps?\nThis application wants to jump from " + appName1() + " to " + appName2() + " Do you wish to proceed? (Y/N)");
+    // ynInput = getChar( );
+    // scanf("%s %d", str, &ynInput);
+    ynInput = (input_method_missing);
+    if (ynInput = "y" or "Y"):
+      return app2URL();
+    else:
+      exit; // Exits the prompt
+}}} // End of main method
+@end
+/* File info
+* File version: `1 (Thursday, March 11th 2021 at 8:27 pm)
+* File type: Objective-C Source file (*.m)
+* Line count (including blank lines and compiler line): 37
+* This script is incomplete, and needs lots of work */
+// End of script
