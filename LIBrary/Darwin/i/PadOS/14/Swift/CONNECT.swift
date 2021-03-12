d0db7630345ca494bcbede3869dfab3a5b9a13f7
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-11 21:21:22 -0800

    CONNECT.swift
    The connection script for iPadOS 14.

---
+++ LIBrary/Darwin/i/PadOS/14/Swift/CONNECT.swift
@@ -0,0 +1,30 @@
+// Start of script
+public class iPadOS14(14.24); { // x = 24 = x, translates to: 14.x
+    // Warning: MacOS programming is different than iOS/iPhoneOS/iPadOS programming. This program has an extra high chance of not working due to missing mobile software libraries
+    // I am assuming iOS 7 is the last version to support AppleScript, and iOS 8 is the first version of iOS to make Swift mandatory when compared to AppleScript.
+    // Connecting 2 iPadOS 14.x. programs together. Note that I have very little experience with Swift, so this script likely doesn't work correctly, and is currently just a demo script
+    //Definitions
+    NSString *str = @"",";
+    str = [str stringByAppendingString:@""];
+    appName1 = str("Enter app name here (the current one."); // Modify this line for your program
+    appName2 = str("Enter second app name here (the one you are linking to."); // Modify this line as well for your program
+    int ynInput; // The input method
+    app2URL = string("/I don't know the iPadOS 14 File path/") // Incomplete. This method is for linking the 2 apps together, and jumping to the other program. Will prompt an install notice if it fails to reach. If the program is installed but reaching it results in an error, that means that ProTraApp was configured incorrectly
+    // Main
+    func switchPrograms() {
+      print ("Switch programs/apps?\nThis application wants to jump from " + appName1() + " to " + appName2() + " Do you wish to proceed? (Y/N)");
+    // ynInput = getChar( );
+    // scanf("%s %d", str, &ynInput);
+    ynInput = (input_method_missing);
+    if (ynInput = "y" or "Y"):
+      return app2URL();
+    else:
+      exit; // Exits the prompt
+    return switchPrograms()
+}} // End of main method
+/* File info
+* File version: `1 (Thursday, March 11th 2021 at 9:21 pm)
+* File type: Swift Source file (*.swift)
+* Line count (including blank lines and compiler line): 31
+* This script is incomplete, and needs lots of work */
+// End of script
