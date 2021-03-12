647a7bf990d8bddcff8d95b62cbce4d1f2d2c549
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-11 22:07:12 -0800

    CONNECT.c
    The connection script for FreeBSD 7.

---
+++ LIBrary/BSD/FreeBSD/x64/7/C/CONNECT.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+// Start of script
+// Connecting 2 FreeBSD 7 apps together (32 bit)
+// Definitions
+union freeBSD_7(int(7)) // Trying to form a class here, so it can detect from different Fedora versions.
+appName1 = char("Enter app name here (the current one."); // Modify this line for your program
+appName2 = char("Enter second app name here (the one you are linking to."); // Modify this line as well for your program
+int ynInput; // The input method
+app2URL = char("/home/<") // Incomplete. This method is for linking the 2 apps together, and jumping to the other program. Will prompt an install notice if it fails to reach. If the program is installed but reaching it results in an error, that means that ProTraApp was configured incorrectly
+exit = endProgram // I don't know how to end the program in C
+// Main
+int main(void); {
+    printf("Switch programs/apps?\nThis application wants to jump from " + appName1() + " to " + appName2() + " Do you wish to proceed? (Y/N)");
+    // ynInput = getChar( );
+    scanf("%s %d", str, &ynInput);
+    if (ynInput = "y" or "Y"); {
+      return app2URL;
+    } else: {
+      return exit; // Exits the prompt
+}} // End of main method
+
+/* File info
+* File version: `1 (Thursday, March 11th 2021 at 10:07 pm)
+* File type: C Source file (*.c)
+* Line count (including blank lines and compiler line): 29
+*/
+// This script is incomplete, and needs lots of work
+// End of script
